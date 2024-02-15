067a4ad8a05dcf59cd14ad935070397b086252c5
John Doe <j.donatmagnin@it-students.fr>
2024-02-15 18:34:24 +0100

    After suppressing password authentification

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, View } from 'react-native';
+import GoalList from './components/GoalList';
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <GoalList />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +21,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
